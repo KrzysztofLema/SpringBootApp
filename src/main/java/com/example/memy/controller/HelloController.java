8afaed521949b74be74a8fb85b628e9532d99f86
Damian <dampiet34@wp.pl>
2017-08-01 17:28:57 +0200

    repair hello

--- src/main/java/com/example/memy/controller/HelloController.java
+++ src/main/java/com/example/memy/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.example.memy.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by slickender on 01.08.2017.
  */
+@Controller
 public class HelloController {
     @GetMapping("/")
     public String hello(ModelMap modelMap){
-        modelMap.addAttribute("person", "Krzysiek");
+        modelMap.addAttribute("hello", "Krzysiek");
         return "hello";
     }
 }
