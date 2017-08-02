58e0896bdbc28ec4792b80c52c2b895513646004
Damian <dampiet34@wp.pl>
2017-08-02 16:57:57 +0200

    adding function to category

--- src/main/java/com/example/memy/ArticleDao/CategoryDao.java
+++ src/main/java/com/example/memy/ArticleDao/CategoryDao.java
@@ -1,6 +1,7 @@
 package com.example.memy.ArticleDao;
 
 import com.example.memy.Model.Category;
+import com.example.memy.Model.Gif;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ import java.util.List;
  */
 public interface CategoryDao {
     List<Category> findAll();
+    Category gifsInCategory(Long id);
 }
--- src/main/java/com/example/memy/Model/Category.java
+++ src/main/java/com/example/memy/Model/Category.java
@@ -1,5 +1,8 @@
 package com.example.memy.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by slickender on 02.08.2017.
  */
@@ -7,6 +10,7 @@ public class Category {
 
     private Long id;
     private String name;
+    private List<Gif> gifs=new ArrayList<>();
 
     public Category() {
     }
@@ -14,6 +18,14 @@ public class Category {
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public List<Gif> getGifs() {
+        return gifs;
+    }
+
+    public void setGifs(List<Gif> gifs) {
+        this.gifs = gifs;
     }
 
     public Long getId() {
@@ -30,5 +42,18 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void addGifsToCattegory(Gif gif){
+        gifs.add(gif);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gifs=" + gifs +
+                '}';
     }
 }
